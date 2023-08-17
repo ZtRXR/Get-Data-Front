c2b783c7b7b2bf4a1a8d3bfd995d865d2326929c
ZtRXR <Zengtudor@outlook.com>
2023-08-17 21:32:51 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { NLayout,NLayoutHeader,NLayoutContent,NLayoutFooter } from "naive-ui"
+
+
+</script>
+
+<template>
+    <n-layout>
+      <n-layout-header style="min-height: 30px;">Header</n-layout-header>
+      <n-layout-content content-style="padding: 24px;" style="min-height: 500px;">
+        Text
+      </n-layout-content>
+      <n-layout-footer>footer</n-layout-footer>
+    </n-layout>
+</template>
+
+<style scoped>
+</style>
